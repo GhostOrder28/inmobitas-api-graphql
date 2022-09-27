534e61eb42a87faaea5cd8ec295abc37fa803143
Ghost Order <kodaichi.kun@gmail.com>
2022-09-27 18:25:19 -0500

    route for serviceworker added

--- src/server.js
+++ src/server.js
@@ -81,6 +81,9 @@ app.use('/checkverified', checkVerifiedRouter);
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+app.get('/service-worker.js', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
 app.use(errorHandler);
 app.use((err, req, res, next) => res.sendStatus(500));
 
