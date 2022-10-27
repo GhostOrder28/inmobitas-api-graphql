785658341ed76f0a003e2e54e3265df009aa93bb
Ghost Order <kodaichi.kun@gmail.com>
2022-10-26 19:46:44 -0500

    rate limit was modified, messages were changed accordingly

--- src/routes/auth/auth.router.js
+++ src/routes/auth/auth.router.js
@@ -14,7 +14,7 @@ const {
 const { checkUserType } = require('../../middlewares/user-type.middlewares');
 
 const apiLimiter = rateLimit({
-	windowMs: 30 * 60 * 1000,
+	windowMs: 60 * 60 * 1000,
 	max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
